78cc30e466ef2b44a606304cf4596d1745138331
Zakia Afrin Jeme <zjeme@techlaunch.io>
2019-03-12 15:06:23 -0400

    first commit

---
+++ precache-manifest.d1beb78af4f8d85b459a5f539a93c0f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ecd8f9059c855ffea022",
+    "url": "/React-Search-App/static/js/runtime~main.ecd8f905.js"
+  },
+  {
+    "revision": "ec81d07732bd9369d801",
+    "url": "/React-Search-App/static/js/main.ec81d077.chunk.js"
+  },
+  {
+    "revision": "0f9f8e5e27ed912971d8",
+    "url": "/React-Search-App/static/js/2.0f9f8e5e.chunk.js"
+  },
+  {
+    "revision": "ec81d07732bd9369d801",
+    "url": "/React-Search-App/static/css/main.4f295106.chunk.css"
+  },
+  {
+    "revision": "c466136aa8115e78f132585b90b749c2",
+    "url": "/React-Search-App/index.html"
+  }
+];
